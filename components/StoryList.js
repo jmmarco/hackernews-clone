774a5eeb61a5fe2b981683db6d248f7ff705adeb
Juan Martin Marco <jmmarco@gmail.com>
2018-05-16 13:59:34 -0300

    Add UI enhancements

--- components/StoryList.js
+++ components/StoryList.js
@@ -1,27 +1,33 @@
-export const StoryList = (props) => {
+import Link from 'next/link'
 
+export const StoryList = (props) => {
+  console.log(props)
   const { story, index } = props
   const {points, user, comments_count, time_ago, domain, url} = story
   const commentUrl = `${url}`
-  console.log(props)
+
   return (
     <div className="story">
-      <span className="index">{index}.</span>
+      {index && <span className="index">{index}.</span>}
       <span className="upvote">&#9652;</span>
-      <a href={story.url}><h2>{story.title}</h2><span className="domain"> ({domain})</span></a>
+      <a href={story.url}>
+        <h2>{story.title}</h2>
+        <span className="domain"> {domain && `(${domain})` }</span>
+      </a>
       <div className="story-info">
-        {points} points by {user} {time_ago} ago | <a href="#">hide</a> | <a href={url}>{comments_count === 0 ? `discuss` : `${comments_count} comments`}</a>
+        {points} points by {user} {time_ago} ago | <a href="#">hide</a> | <Link prefetch href={`/story?id=${story.id}`}><a>{comments_count === 0 ? `discuss` : `${comments_count} comments`}</a></Link>
       </div>
       <style jsx>{`
         .story {
           margin: 0;
-          padding: 5px;
+          padding: 0 5px 5px 5px;
           font-family: Verdana, Geneva, sans-serif;
-          font-size: 10px;
+          font-size: 10pt;
         }
         .story a {
           text-decoration: none;
           color: black;
+
         }
 
         .story h2 {
@@ -35,11 +41,12 @@ export const StoryList = (props) => {
           font-size: 14pt;
         }
 
-        .story-info, .domain {
+        .story-info, .domain, .index {
           color: #828282;
         }
         .story-info {
           font-size: 7pt;
+          padding-left:20px;
         }
 
         .domain {
@@ -52,11 +59,5 @@ export const StoryList = (props) => {
 
 
 }
-
-
-
-
-
-
 
 export default StoryList
